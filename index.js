3ba0fd9f2090310f69b56a93de249adcd0aa082f
WillHubner <willian.hubner@hotmail.co.uk>
2024-11-06 15:49:24 -0400

    upload inicial 4

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-function gerarPDF() {    
-    const element = document.getElementById('content');
-
-    const options = {
-        margin: 1,             // Margens do PDF em cm
-        filename: 'documento.pdf',   // Nome do arquivo gerado
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'cm', format: 'a4', orientation: 'portrait' }
-    };
-    
-    html2pdf().set(options).from(element).toPdf().output('blob').then((pdfBlob) => {
-
-        console.log('abracadabra');
-
-        const file = new File([pdfBlob], 'documento.pdf', { type: 'application/pdf' });
-          
-        if (navigator.canShare) {            
-            navigator.share({
-                title: 'Compartilhar PDF',
-                files: [file]
-            }).then(() => {
-                console.log('PDF compartilhado com sucesso!');
-            }).catch((error) => {
-                console.error('Erro ao compartilhar:', error);
-            });
-        } else {
-            console.log('O compartilhamento de arquivos não é suportado neste dispositivo.');
-        }
-    });    
-}
+$('#btnConsultar').click(function() {
+    window.location.href = 'geradorpdf.html';
+});
